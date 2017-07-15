ce6ced7de7b8c827b67547126f041d5f3bf3d4d6
Uice Lu <uicestone@gmail.com>
2017-07-15 14:08:24 +0800

    fix: 菜单进入练习未进入第一题

--- wp-content/themes/coursaty/archive-exercise.php
+++ wp-content/themes/coursaty/archive-exercise.php
@@ -1,8 +1,12 @@
 <?php
 
-the_post();
+$ordered_exercises = new WP_Query($query_string."&orderby=ID&order=ASC&posts_per_page=1");
+
+if ($ordered_exercises->have_posts()): $ordered_exercises->the_post();
 
 header('Location: ' . get_the_permalink());
+
+endif;
 
 ?>
 
