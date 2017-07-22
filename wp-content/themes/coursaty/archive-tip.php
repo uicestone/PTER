fcf50e0676f5c8ed7b259d75bee9a50e82c2d589
Uice Lu <uicestone@gmail.com>
2017-07-22 20:46:58 +0800

    技巧列表不显示详情

--- wp-content/themes/coursaty/archive-tip.php
+++ wp-content/themes/coursaty/archive-tip.php
@@ -20,10 +20,29 @@
                     <img src="<?=get_stylesheet_directory_uri()?>/assets/img/icons/addcourse-icon.png" alt="" class="fl add-courses-icon">
                     <span class="add-courses-title ln-tr"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span>
                     <div class="content">
-						<?php the_content(); ?>
+						<?php the_excerpt(); ?>
                     </div>
                 </div>
                 <?php endwhile; ?>
+                <div class="col-md-12 pagination">
+					<?php
+
+					$paginate_links = paginate_links(array(
+							'base' => str_replace( 99999, '%#%', esc_url( get_pagenum_link( 99999 ) ) ),
+							'type' => 'list',
+							'format'=>'/%n%/page/%#%',
+							'prev_text' => '&laquo;',
+							'next_text' => '&raquo;',
+							'current' => max( 1, get_query_var('paged') ),
+							'total' => $wp_query->max_num_pages)
+					);
+
+					$paginate_links = str_replace('<li>', '<li class="ib">', $paginate_links);
+
+					echo $paginate_links;
+
+					?>
+                </div>
             </div><!-- End row -->
         </div><!-- End Container -->
     </div><!-- End Latest-Courses Section Content -->
