e4cbac862e37bcb3090aa96f2898d30c279cefaa
Uice Lu <uicestone@gmail.com>
2017-07-27 21:04:12 +0800

    网页底部微信二维码

--- wp-content/themes/coursaty/footer.php
+++ wp-content/themes/coursaty/footer.php
@@ -5,45 +5,16 @@
 		<div class="row">
 			<div class="col-md-3">
 				<div class="widget about-widget">
-                    <?php $about = get_posts(array('name' => 'about'))[0]; ?>
-					<h6 class="widget-title"><?=get_the_title($about)?></h6>
-					<p class="about-text">
-						<?=$about->post_excerpt?>
-					</p>
-					<div class="footer-links">
-						<ul>
-                            <li><a href="<?=site_url()?>/agreement/">使用协议</a></li>
-                            <li><a href="<?=site_url()?>/how-to-use/">使用指南</a></li>
-							<li><a href="<?=site_url()?>/about/" class="ln-tr">关于我们</a></li>
-							<li><a href="<?=site_url()?>/contact/">联系我们</a></li>
-						</ul>
-					</div><!-- End Footer Links -->
+					<h6 class="widget-title">微信客服号</h6>
+					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/qrcode_cs.jpg">
 				</div><!-- End About Widget -->
 			</div><!-- End col-md3 -->
 			<div class="col-md-3">
 				<div class="widget courses-widget">
-					<h6 class="widget-title">最新文章</h6>
-					<div id="footer-courses-slider" class="flexslider">
-						<ul class="slides">
-                            <?php foreach (get_posts() as $index => $post): ?>
-                            <?php if ($index % 2 === 0): ?>
-                            <li class="clearfix">
-                            <?php endif; ?>
-                            <div class="course-icon fl">
-                                <span class="icon grad-btn"><i class="fa fa-bookmark"></i></span>
-                            </div><!-- End Course Icon -->
-                            <div class="course-info">
-                                <h4 class="footer-course-title"><a href="<?=get_the_permalink($post)?>" class="ln-tr"><?=get_the_title($post)?></a></h4>
-                                <span class="course-date"><?=get_the_date('', $post)?></span>
-                            </div><!-- End Course Info -->
-                            <?php if ($index % 2 === 1): ?>
-                            </li>
-                            <?php endif; ?>
-                            <?php endforeach; ?>
-						</ul><!-- End ul Items -->
-					</div><!-- End Footer Scourses Slider -->
+					<h6 class="widget-title">微信公众号</h6>
+                    <img src="<?=get_stylesheet_directory_uri()?>/assets/img/qrcode_mp.jpg">
 				</div><!-- End Courses Widget -->
-	g		</div><!-- End col-md4 -->
+			</div><!-- End col-md4 -->
 			<div class="col-md-6">
 				<div class="widget">
                     <?php if ($copyright = get_posts('name=copyright')[0]): ?>
