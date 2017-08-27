afa0070a3154104564bc5b2a9fe6e7e04b498af6
Uice Lu <uicestone@gmail.com>
2017-08-27 18:09:54 +0800

    fix: Apache遇到带var文件名出错

--- wp-content/themes/coursaty/functions.php
+++ wp-content/themes/coursaty/functions.php
@@ -16,7 +16,7 @@ add_action('wp', function() {
 	wp_register_script('waypoints', get_stylesheet_directory_uri() . '/assets/js/jquery.waypoints.min.js', array('jquery'), '3.0.0', true);
 	wp_register_script('sticky', get_stylesheet_directory_uri() . '/assets/js/jquery.sticky.min.js', array('jquery'), '1.0.4', true);
 	wp_register_script('jsdiff', get_stylesheet_directory_uri() . '/assets/js/jsdiff.js', array(), '3.3.0', true);
-	wp_register_script('waveform', get_stylesheet_directory_uri() . '/assets/js/vendor/waveform/waveform-playlist.var.js', array(), '3.0.4', true);
+	wp_register_script('waveform', get_stylesheet_directory_uri() . '/assets/js/vendor/waveform/waveform-playlist.js', array(), '3.0.4', true);
 	wp_register_script('waveform-record', get_stylesheet_directory_uri() . '/assets/js/vendor/waveform/record.js', array(), '3.0.4', true);
 	wp_register_script('waveform-emitter', get_stylesheet_directory_uri() . '/assets/js/vendor/waveform/emitter.js', array(), '3.0.4', true);
 	wp_register_script('scripts', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0.2', true);
