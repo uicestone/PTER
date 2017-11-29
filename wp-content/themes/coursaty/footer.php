fc9ed848bf8efd5f63e9734df6002c383e470737
Uice Lu <uicestone@gmail.com>
2017-11-29 14:14:55 +0800

    二维码变更

--- wp-content/themes/coursaty/footer.php
+++ wp-content/themes/coursaty/footer.php
@@ -6,7 +6,7 @@
 			<div class="col-md-3 col-sm-6">
 				<div class="widget about-widget">
 					<h6 class="widget-title">微信客服号</h6>
-					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/qrcode_cs.jpg">
+					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/qrcode_mp_co.jpg">
 				</div><!-- End About Widget -->
 			</div><!-- End col-md3 -->
 			<div class="col-md-3 col-sm-6">
