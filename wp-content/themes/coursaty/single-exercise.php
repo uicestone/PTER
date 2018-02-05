0c5fa6fbf222ee2666fa4f59b6dae881ed773c65
Uice Lu <uicestone@gmail.com>
2018-02-06 01:21:39 +0800

    选择题选项输出

--- wp-content/themes/coursaty/single-exercise.php
+++ wp-content/themes/coursaty/single-exercise.php
@@ -209,6 +209,29 @@ get_header(); ?>
 							</form><!-- End form -->
 						</div><!-- End comment form -->
 						<?php endif; ?>
+						<?php if (in_array($question_type->slug, array('multiple-choice-reading', 'multiple-choice-listening'))): ?>
+							<div class="clearfix" style="margin-top:30px"></div>
+							<div class="comment-form answer-form entry">
+								<div class="addcomment-title">
+									<span class="icon"><i class="fa fa-comments-o"></i></span>
+									<span class="text">你的选择</span>
+								</div><!-- End Title -->
+								<form method="post" action="/" id="answer-form">
+									<div class="row">
+										<div class="col-md-12">
+											<div class="input row">
+												<?php foreach(explode("\n", get_field('choices')) as $index => $choice): ?>
+												<label class="col-sm-2" style="cursor:pointer">
+													<input name="answer" value="<?=$choice?>" type="<?=get_field('multiple')?'checkbox':'radio'?>">
+													<?=$index+1?>: <?=$choice?>
+												</label>
+												<?php endforeach; ?>
+											</div>
+										</div>
+									</div>
+								</form><!-- End form -->
+							</div><!-- End comment form -->
+						<?php endif; ?>
 						<?php if (empty($exam)): ?>
                         <div class="comment-form comments-list entry answer">
                             <div class="addcomment-title" style="margin-bottom:20px">
