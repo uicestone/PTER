f7d77122dce2d4db53bc94cd1026526ab6ad8578
Uice Lu <uicestone@gmail.com>
2018-02-06 00:10:11 +0800

    SendGrid统计汉化

--- wp-content/plugins/sendgrid-email-delivery-simplified/view/partials/sendgrid_stats_widget.php
+++ wp-content/plugins/sendgrid-email-delivery-simplified/view/partials/sendgrid_stats_widget.php
@@ -2,14 +2,14 @@
   <div class="sendgrid-filters">
     <input type="hidden" id="sendgrid-statistics-type" name="sendgrid-statistics-type" value="wordpress" />
     <div class="pull-left">
-      <label for="sendgrid-start-date">Start date</label>
+      <label for="sendgrid-start-date">开始日期</label>
       <input type="text" id="sendgrid-start-date" name="sendgrid_start_date" />
     </div>
     <div class="pull-left">
-      <label for="sendgrid-end-date">End date</label>
+      <label for="sendgrid-end-date">结束日期</label>
       <input type="text" id="sendgrid-end-date" name="sendgrid_end_date" />
     </div>
-    <a href="#" id="sendgrid-apply-filter" data-filter="<?php if ( isset( $_GET['page'] ) and "sendgrid-statistics" == sanitize_text_field( $_GET['page'] ) ) { ?>sendgrid-statistics<?php } else { ?>dashboard<?php } ?>" class="button button-primary">Apply</a>
+    <a href="#" id="sendgrid-apply-filter" data-filter="<?php if ( isset( $_GET['page'] ) and "sendgrid-statistics" == sanitize_text_field( $_GET['page'] ) ) { ?>sendgrid-statistics<?php } else { ?>dashboard<?php } ?>" class="button button-primary">查询</a>
   </div>
   <div class="loading"><img src="<?php echo plugin_dir_url( __FILE__ ); ?>../images/loader.gif" style="width: 15px; height: 15px;" /></div>
 </div>
@@ -19,24 +19,24 @@
   <?php echo ( version_compare( get_bloginfo( "version" ), '3.7.10', '>' ) ? "wordpress-new" : ""); ?>" style="position:relative;">
   <div class="widget others" id="deliveries">
     <div class="widget-top">
-      <div class="widget-title"><h4>Deliveries</h4></div>
+      <div class="widget-title"><h4>发送</h4></div>
     </div>
     <div class="widget-inside">
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(50,135,1);"></span><span>Requests</span>
+          <span class="square" style="background-color: rgb(50,135,1);"></span><span>请求</span>
         </div>
         <div id="requests" class="pull-right">0</div>
       </div>
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(188,213,22);"></span><span>Drop</span>
+          <span class="square" style="background-color: rgb(188,213,22);"></span><span>丢弃</span>
         </div>
         <div id="drop" class="pull-right">0%</div>
       </div>
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(251,166,23);"></span><span>Delivered</span>
+          <span class="square" style="background-color: rgb(251,166,23);"></span><span>送达</span>
         </div>
         <div id="delivered" class="pull-right">0%</div>
       </div>
@@ -45,24 +45,24 @@
 
   <div class="widget others" id="compliance">
     <div class="widget-top">
-      <div class="widget-title"><h4>Compliance</h4></div>
+      <div class="widget-title"><h4>投诉</h4></div>
     </div>
     <div class="widget-inside">
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(251,229,0);"></span><span>Spam Reports</span>
+          <span class="square" style="background-color: rgb(251,229,0);"></span><span>垃圾邮件报告</span>
         </div>
         <div id="spam-reports" class="pull-right">0%</div>
       </div>
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(17,133,193);"></span><span>Bounces</span>
+          <span class="square" style="background-color: rgb(17,133,193);"></span><span>退回</span>
         </div>
         <div id="bounces" class="pull-right">0%</div>
       </div>
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(188,208,209);"></span><span>Blocks</span>
+          <span class="square" style="background-color: rgb(188,208,209);"></span><span>被屏蔽</span>
         </div>
         <div id="blocks" class="pull-right">0%</div>
       </div>
@@ -71,30 +71,30 @@
 
   <div class="widget others" id="engagement">
     <div class="widget-top">
-      <div class="widget-title"><h4>Engagement</h4></div>
+      <div class="widget-title"><h4>参与度</h4></div>
     </div>
     <div class="widget-inside">
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(62,68,192);"></span><span>Unsubscribes</span>
+          <span class="square" style="background-color: rgb(62,68,192);"></span><span>取消订阅</span>
         </div>
         <div id="unsubscribes" class="pull-right">0%</div>
       </div>
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(255,0,224);"></span><span>Unique Opens</span>
+          <span class="square" style="background-color: rgb(255,0,224);"></span><span>独立打开</span>
         </div>
         <div id="unique-opens" class="pull-right">0%</div>
       </div>
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(224,68,40);"></span><span>Opens</span>
+          <span class="square" style="background-color: rgb(224,68,40);"></span><span>打开</span>
         </div>
         <div id="opens" class="pull-right">0%</div>
       </div>
       <div class="row clearfix">
         <div class="pull-left">
-          <span class="square" style="background-color: rgb(50,135,1);"></span><span>Clicks</span>
+          <span class="square" style="background-color: rgb(50,135,1);"></span><span>点击</span>
         </div>
         <div id="clicks" class="pull-right">0%</div>
       </div>
@@ -103,7 +103,7 @@
   <br class="clearfix-clear"/>
 
   <?php if ( ! isset ($_GET['page'] ) or "sendgrid-statistics" != sanitize_text_field( $_GET['page'] ) ) { ?>
-    <a href="index.php?page=sendgrid-statistics" class="more-statistics">See charts</a>
+    <a href="index.php?page=sendgrid-statistics" class="more-statistics">查看图表</a>
     <br class="clearfix-clear"/>
   <?php } ?>
 </div>
