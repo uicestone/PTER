88836229b0109186258b64f752a855b747d72add
Uice Lu <uicestone@gmail.com>
2018-03-25 00:23:40 +0800

    合并内容顶部版权

--- wp-content/themes/coursaty/single-exercise.php
+++ wp-content/themes/coursaty/single-exercise.php
@@ -97,29 +97,14 @@ if ($_GET['random']) {
 		$random_exercise = $random_exercises[0];
 	}
 }
-elseif (isset($_GET['section'])) {
-	// did everything in single-exam.php
-}
-else {
+elseif (empty($_GET['section'])) {
 	$previous_exercise = get_previous_post(true, '', $_GET['tag'] ? 'post_tag' : 'question_type');
 	$next_exercise = get_next_post(true, '', $_GET['tag'] ? 'post_tag' : 'question_type');
 }
 
 get_header(); ?>
 
-<div class="copyright-header container">
-    <p>All Rights Reserved &copy; Bingo Training Pty. Ltd. ABN 64 618 887 951, ACN 618 887 951</p>
-	<?php if (is_limited_free(get_current_user_id())): ?>
-        <hr>
-        <p style="font-weight:normal">
-            您是限时免费订阅用户，将于
-			<?php $expires_at = get_user_meta(get_current_user_id(), 'service_tips_valid_before', true); ?>
-            <span class="count-down" data-expires-at="<?=$expires_at?>">
-            <?=date('Y-m-d H:i:s', $expires_at + get_option( 'gmt_offset' ) * HOUR_IN_SECONDS)?>
-        </span>
-            过期，请了解我们的<a href="<?=site_url('profile')?>" target="_blank">优惠政策</a>，并及时<a href="<?=site_url('pricing-table')?>">订阅</a></p>
-	<?php endif; ?>
-</div>
+<?php get_template_part('content-top-copyright'); ?>
 
 <article class="post single exercise">
 	<div class="container">
@@ -320,7 +305,7 @@ get_header(); ?>
 							</form>
 						</div>
 						<?php endif; ?>
-						<?php if (empty($_GET['finish']) && $section !== 'break'): ?>
+						<?php if (empty($_GET['finish']) && $section !== 'break' && $section_time_left >= 0): ?>
 						<div class="col-md-6" style="padding-right:15px;padding-left:5px;<?=$answer ? 'display:none;' : ''?>">
 							<button type="submit" class="btn primary-btn submit-answer">
 								<i class="fa fa-hand-paper-o"></i>
