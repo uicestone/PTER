e82dbceb5d9f6888143a0d710ba2dda26d5cd1c8
Uice Lu <uicestone@gmail.com>
2018-03-30 23:50:31 +0800

    关键字和描述

--- wp-content/themes/coursaty/header.php
+++ wp-content/themes/coursaty/header.php
@@ -7,7 +7,8 @@
 	<!-- Meta Tags -->
 	<meta charset="utf-8">
 	<title><?php wp_title('-', true, 'right'); bloginfo('name'); echo ' - '; bloginfo('description') ?></title>
-	<meta name="description" content="<?php bloginfo('description'); ?>">
+	<meta name="keywords" content="PTE练习 PTE技巧 PTE考试 PTE模考">
+	<meta name="description" content="<?php is_single() ? print(get_the_excerpt()) : bloginfo('description'); ?>">
 	<meta name="author" content="Bingo Training Pty Ltd, Uice Lu">
 	<!-- Mobile Meta -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
