fe88e15bbb22e327010d70cd463732add7182a86
Uice Lu <uicestone@gmail.com>
2018-03-31 00:02:34 +0800

    更改为主题项目, 不在包含WordPress主程序

--- index.php
+++ index.php
@@ -1,17 +1,53 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php get_header(); ?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+<section class="blog listview">
+	<div class="container">
+		<div class="row">
+			<?php if(have_posts()): while (have_posts()): the_post(); ?>
+			<div class="col-md-12">
+				<div class="blogpost clearfix fadeInDown-animation">
+					<div class="blogpost-image">
+						<?php the_post_thumbnail('post-thumbnail', array('class' => 'img-responsive')); ?>
+					</div><!-- End Post Image -->
+					<div class="blogpost-info clearfix">
+						<h3 class="blogpost-title fl">
+							<span class="post-type-icon"><i class="fa fa-comment"></i></span>
+							<a href="<?php the_permalink(); ?>" class="ln-tr"><?php the_title(); ?></a>
+						</h3>
+						<p class="blogpost-description">
+							<?php the_excerpt(); ?>
+						</p>
+						<div class="buttons fr">
+							<a href="<?php the_permalink(); ?>" class="btn grad-btn orange-btn read-btn">查看详情</a>
+						</div>
+					</div>
+				</div><!-- End post -->
+			</div><!-- End col-md-12 -->
+			<?php endwhile; ?>
+			<div class="col-md-12 pagination">
+				<?php
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+				$paginate_links = paginate_links(array(
+					'base' => str_replace( 99999, '%#%', esc_url( get_pagenum_link( 99999 ) ) ),
+					'type' => 'list',
+					'format'=>'/%n%/page/%#%',
+					'prev_text' => '&laquo;',
+					'next_text' => '&raquo;',
+					'current' => max( 1, get_query_var('paged') ),
+					'total' => $wp_query->max_num_pages)
+				);
+
+				$paginate_links = str_replace('<li>', '<li class="ib">', $paginate_links);
+
+				echo $paginate_links;
+
+				?>
+			</div>
+			<?php else: ?>
+				<div style="text-align:center">没有找到任何内容</div>
+			<?php endif; ?>
+		</div><!-- End row -->
+	</div><!-- End container -->
+</section><!-- End Blog 1 Listview -->
+
+<?php get_footer(); ?>
--- wp-content/plugins/weixin-api/template/transfer_customer_service.php
+++
@@ -1,6 +0,0 @@
-<xml>
-	<ToUserName><![CDATA[<?=$received_message['FROMUSERNAME']?>]]></ToUserName>
-	<FromUserName><![CDATA[<?=$received_message['TOUSERNAME']?>]]></FromUserName>
-	<CreateTime><?=time()?></CreateTime>
-	<MsgType><![CDATA[transfer_customer_service]]></MsgType>
-</xml>
