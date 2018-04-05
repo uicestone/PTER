5ac30f64a1c51d32e212805a15942ded989388ff
Uice Lu <uicestone@gmail.com>
2018-04-05 16:27:47 +0800

    移动端加载桌面Banner

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<?php $banners = get_posts(array('tag' => 'headline')); ?>
+<?php $banners = get_posts(array('tag' => 'headline')); if (!wp_is_mobile()): ?>
 <div class="tp-banner-container">
     <div class="tp-banner">
         <ul>
@@ -58,8 +58,7 @@
         </ul><!-- end ul elements -->
     </div><!-- end tp-banner -->
 </div><!-- End Home Slider Container -->
-
-<?php foreach ($banners as $index => $banner): ?>
+<?php else: foreach ($banners as $index => $banner): ?>
 <section class="full-section banner" style="background-image:url('<?=get_the_post_thumbnail_url($banner, 'large')?>')">
     <div class="container">
         <h3 class="section-title"><?=get_the_title($banner)?></h3>
@@ -78,7 +77,7 @@
     </div>
 </section>
 <div class="clearfix"></div>
-<?php endforeach; ?>
+<?php endforeach; endif; ?>
 
 <section class="full-section features-section fancy-shadow pricing-tables">
     <div class="container">
