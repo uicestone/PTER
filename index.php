4e3c54140adf00073b40403dbe084a03d63f3f34
Uice Lu <uicestone@gmail.com>
2018-07-16 16:42:31 +0800

    fix: blog css 类名冲突

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section class="blog listview">
+<section class="blog-section listview">
 	<div class="container">
 		<div class="row">
 			<?php if(have_posts()): while (have_posts()): the_post(); ?>
